4f5edf8b924c02de24de4ae0dcac2cb264e3eb49
João Victor <elietejvc@hotmail.com>
2025-10-07 23:23:38 -0300

    feat: chamando rotas

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
+	"github.com/joaovictorcruz/golang-api/src/controller/routes"
 	"github.com/joho/godotenv"
 )
 
@@ -10,5 +12,15 @@ func main(){
 	err := godotenv.Load()
 	if err != nil{
 		log.Fatal("Error loading .env file")
+	}
+
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	router.Run(":8080")
+	
+	if err = router.Run(":8080"); err != nil{
+		log.Fatal(err)
 	}
 }
